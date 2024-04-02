6fa9fca818ccf0c7a0e463a670b8086558c3dbd1
James <collj322@gmail.com>
2024-04-02 18:49:12 +0100

    Initial commit

---
+++ Grayscale.py
@@ -0,0 +1,28 @@
+# Import opencv
+import cv2
+
+print("Please enter the file location of the image to convert to grayscale!")
+image_location = input("The location: ")
+
+# Load the input image
+img = cv2.imread(image_location)
+
+# Obtain the dimensions of the image array using the shape method
+(row, col) = img.shape[0:2]
+
+# Take the average of pixel values of the BGR Channels
+# to convert the colored image to grayscale image
+for i in range(row):
+    for j in range(col):
+        # Find the average of the BGR pixel values
+        img[i, j] = sum(img[i, j]) * 0.33
+
+print("Do you wish to save the image?")
+in2 = int(input("1 to save, 2 for no: "))  # Convert input to integer
+if in2 == 1:
+    in3 = input("Please enter an image name: ")
+    cv2.imwrite(in3 + '.jpg', img)  # Save as JPEG
+
+elif in2 == 2:
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
